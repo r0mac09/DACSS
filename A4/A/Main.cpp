3528f76bae7f0bca76e8e18830397c8fe1680f31
Alexandru Mihaescu <mihaescuac@gmail.com>
2019-04-29 16:03:50 +0300

    Added reqs

--- A4/A/Main.cpp
+++ A4/A/Main.cpp
@@ -7,6 +7,14 @@
 // according to
 // https://www.tutorialspoint.com/design_pattern/data_access_object_pattern.htm
 
+// REQ: Design a Horoscope application, which calculates the horoscope of students.
+// The students can be read from different data sources (XML files, SQL database).
+// Use the Data Acces Object pattern to decouple between the business logic and the
+// data source. The business logic (the horoscope algorithm) predicts if a student
+// will have a good day, according to following algorithm: if the number of vowels
+// in the student name is a divisor of the current day, then the student will have
+// a good day. 
+
 int main(void) {
     StudentDaoImpl *dao = new StudentDaoImpl();
 
